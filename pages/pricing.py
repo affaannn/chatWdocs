44ee8fdb07539a1eb57e4dcaa38bbe0f85d53d17
Md Affan <99633856+affaannn@users.noreply.github.com>
2024-07-30 13:56:13 +0530

    Update pricing.py

--- pages/pricing.py
+++ pages/pricing.py
@@ -1,3 +1,20 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 def pricing():
-    st.markdown("<h1 style='text-align: center;'>Find a perfect plan for your use</h1>", unsafe_allow_html=True)
+    st.markdown(
+        """
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Sora:wght@400;700&display=swap');
+        body, h1, h2, h3, h4, h5, h6, p, div, span, a, input, button {
+            font-family: 'Sora', sans-serif;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    st.markdown(
+    "<h1 style='text-align: center; color: rgb(41, 86, 160);'>Find a perfect plan for your use</h1>", 
+    unsafe_allow_html=True)
+
+    
